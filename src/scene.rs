7dba3aa43def329e1ac3deed10e3bc1a6cd4d0dd
FlashOnFire_ <flashonfire.dev@gmail.com>
2024-05-14 18:12:49 +0200

    chore: cleaning

--- src/scene.rs
+++ src/scene.rs
@@ -1,27 +1,33 @@
 use std::fs;
 use std::path::PathBuf;
 
+struct FileEntry {
+    name: String,
+    offset: u32,
+    size: u32,
+}
+
 pub struct Scene {}
 
 impl Scene {
     pub fn from(file: &PathBuf) -> Result<Self, Box<dyn std::error::Error>> {
         println!("Unpacking...");
         let data: Vec<u8> = fs::read(file)?;
+        let mut position: usize = 0;
 
         println!("Data Length : {}", data.len());
-
-        read_header(&data, 0);
+        let file_count = read_header(&data, &mut position);
 
         Ok(Scene {})
     }
 }
 
-fn read_str(data: &[u8], position: usize) -> (String, usize) {
+fn read_str(data: &[u8], position: &mut usize) -> String {
     let iter = data
         .iter()
-        .skip(position);
+        .skip(*position);
 
-    let (size, position) = read_u32(data, position);
+    let size = read_u32(data, position);
 
     let bytes: Vec<u8> = iter
         .clone()
@@ -29,33 +35,32 @@ fn read_str(data: &[u8], position: usize) -> (String, usize) {
         .take(size as usize)
         .copied()
         .collect();
-    
-    let string = String::from_utf8(bytes).unwrap();
-    (string, position + size as usize)
+    *position += size as usize;
+
+    String::from_utf8(bytes).unwrap()
 }
 
-fn read_u32(data: &[u8], position: usize) -> (u32, usize) {
+fn read_u32(data: &[u8], position: &mut usize) -> u32 {
     let first_4_bytes = data
         .iter()
-        .skip(position)
+        .skip(*position)
         .clone()
         .take(4)
         .copied()
         .collect::<Vec<u8>>()
         .try_into()
         .unwrap();
+    *position += 4;
 
-    let size = u32::from_le_bytes(first_4_bytes);
-
-    (size, position+4)
+    u32::from_le_bytes(first_4_bytes)
 }
 
-fn read_header(data: &[u8], position: usize) -> (u32, usize) {
-    let (version, position) = read_str(data, position);
+fn read_header(data: &[u8], position: &mut usize) -> u32 {
+    let version= read_str(data, position);
     assert_eq!(version, "PKGV0001");
 
-    let (file_count, position) = read_u32(data, position);
+    let file_count = read_u32(data, position);
     println!("PKGV0001 - File count : {file_count}");
 
-    (file_count, position)
+    file_count
 }
