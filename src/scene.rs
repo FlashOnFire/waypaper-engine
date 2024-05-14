87b6a86e32ba6645ee0dda10fffb477fd521245a
FlashOnFire_ <flashonfire.dev@gmail.com>
2024-05-14 17:57:31 +0200

    scene: header parsing and file count

--- src/scene.rs
+++ src/scene.rs
@@ -8,20 +8,36 @@ impl Scene {
         println!("Unpacking...");
         let data: Vec<u8> = fs::read(file)?;
 
-        println!("{}", data.len());
+        println!("Data Length : {}", data.len());
 
-        read_str(&data, 0);
+        read_header(&data, 0);
 
         Ok(Scene {})
     }
 }
 
-fn read_str(data: &[u8], position: usize) -> String {
+fn read_str(data: &[u8], position: usize) -> (String, usize) {
     let iter = data
         .iter()
         .skip(position);
 
-    let first_4_bytes = iter
+    let (size, position) = read_u32(data, position);
+
+    let bytes: Vec<u8> = iter
+        .clone()
+        .skip(4)
+        .take(size as usize)
+        .copied()
+        .collect();
+    
+    let string = String::from_utf8(bytes).unwrap();
+    (string, position + size as usize)
+}
+
+fn read_u32(data: &[u8], position: usize) -> (u32, usize) {
+    let first_4_bytes = data
+        .iter()
+        .skip(position)
         .clone()
         .take(4)
         .copied()
@@ -30,17 +46,16 @@ fn read_str(data: &[u8], position: usize) -> String {
         .unwrap();
 
     let size = u32::from_le_bytes(first_4_bytes);
-    println!("{}", size);
 
-    let bytes: Vec<u8> = iter
-        .clone()
-        .skip(4)
-        .take(size as usize)
-        .copied()
-        .collect();
+    (size, position+4)
+}
 
-    let string = String::from_utf8(bytes).unwrap();
+fn read_header(data: &[u8], position: usize) -> (u32, usize) {
+    let (version, position) = read_str(data, position);
+    assert_eq!(version, "PKGV0001");
 
-    println!("{:?}", string);
-    string
+    let (file_count, position) = read_u32(data, position);
+    println!("PKGV0001 - File count : {file_count}");
+
+    (file_count, position)
 }
