d19f41fbbc54e7e2f172977de3e3749b7f3f2ee1
eymeric <eymericdechelette@gmail.com>
2024-05-22 19:41:41 +0200

    chore: Update flake deps for freeimage

--- packaging/nix/default.nix
+++ packaging/nix/default.nix
@@ -6,11 +6,26 @@
   glew-egl,
   libxkbcommon,
   mpv,
+  jxrlib,
+  zlib,
+  libwebp,
+  libtiff,
+  libpng,
+  libraw,
+  openjpeg,
+  openexr,
+  imath,
 }:
 rustPlatform.buildRustPackage {
   pname = "waypaper-engine";
   version = "0.0";
-  cargoLock.lockFile = ../../Cargo.lock;
+  cargoLock = {
+    lockFile = ../../Cargo.lock;
+    outputHashes = {
+      "freeimage-sys-3.18.4" = "sha256-+S5G6cFAfoJUe9h5EgUwWFHZeBWZ5EpQJfljwX52Olk=";
+    };
+  };
+
   src = lib.cleanSource ../..;
 
   buildInputs = [
@@ -18,6 +33,15 @@ rustPlatform.buildRustPackage {
     glew-egl
     libxkbcommon
     mpv
+    jxrlib
+    zlib
+    libwebp
+    libtiff
+    libpng
+    libraw
+    openjpeg
+    openexr
+    imath
   ];
 
   nativeBuildInputs = [
