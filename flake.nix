5f6eba19194942ea3822e27e3ec960adf0db12e3
uku <hi@uku.moe>
2025-07-06 23:19:18 +0200

    fix(nix): use rust-overlay

--- flake.nix
+++ flake.nix
@@ -6,38 +6,73 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-parts,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux"];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-parts,
+      rust-overlay,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-      perSystem = {
-        self',
-        pkgs,
-        system,
-        ...
-      }: {
-        packages = {
-          daemon = pkgs.callPackage ./packaging/nix/daemon.nix {inherit self;};
-          ui = pkgs.callPackage ./packaging/nix/ui.nix {inherit self;};
-          cli = pkgs.callPackage ./packaging/nix/cli.nix {inherit self;};
+      perSystem =
+        {
+          self',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            config = { };
+            overlays = [ rust-overlay.overlays.default ];
+          };
+
+          packages =
+            let
+              rust = pkgs.rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+              };
+
+              rustPlatform = pkgs.makeRustPlatform {
+                rustc = rust;
+                cargo = rust;
+              };
+            in
+            {
+              daemon = pkgs.callPackage ./packaging/nix/daemon.nix { inherit self rustPlatform; };
+              ui = pkgs.callPackage ./packaging/nix/ui.nix { inherit self rustPlatform; };
+              cli = pkgs.callPackage ./packaging/nix/cli.nix { inherit self rustPlatform; };
+            };
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = with self'.packages; [
+              daemon
+              ui
+            ];
+            packages = with pkgs; [
+              clippy
+              rustfmt
+            ];
+
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          };
+
+          formatter = pkgs.nixfmt-tree;
         };
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = with self'.packages; [daemon ui];
-          packages = with pkgs; [clippy rustfmt];
-
-          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-        };
-
-        formatter = pkgs.alejandra;
-      };
     };
 }
--- packaging/nix/daemon.nix
+++ packaging/nix/daemon.nix
@@ -8,45 +8,46 @@
   mold-wrapped,
   pkg-config,
   wayland,
-}: let
+}:
+let
   cargoRoot = "waypaper_engine_daemon";
   cargoToml = lib.importTOML ../../${cargoRoot}/Cargo.toml;
   rev = self.shortRev or self.dirtyShortRev or "dirty";
 in
-  rustPlatform.buildRustPackage {
-    pname = cargoToml.package.name;
-    version = "${cargoToml.package.version}+git.${rev}";
+rustPlatform.buildRustPackage {
+  pname = cargoToml.package.name;
+  version = "${cargoToml.package.version}+git.${rev}";
 
-    src = lib.cleanSource ../..;
+  src = lib.cleanSource ../..;
 
-    inherit cargoRoot;
-    buildAndTestSubdir = cargoRoot;
+  inherit cargoRoot;
+  buildAndTestSubdir = cargoRoot;
 
-    cargoLock.lockFile = ../../Cargo.lock;
+  cargoLock.lockFile = ../../Cargo.lock;
 
-    nativeBuildInputs = [
-      mold-wrapped
-      pkg-config
-      rustPlatform.bindgenHook
-    ];
+  nativeBuildInputs = [
+    mold-wrapped
+    pkg-config
+    rustPlatform.bindgenHook
+  ];
 
-    buildInputs = [
-      ffmpeg-full
-      wayland
-      libGL
-      libxkbcommon
-    ];
+  buildInputs = [
+    ffmpeg-full
+    wayland
+    libGL
+    libxkbcommon
+  ];
 
-    env.RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+  env.RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
 
-    postPatch = ''
-      ln -s ${../../Cargo.lock} ${cargoRoot}/Cargo.lock
-    '';
+  postPatch = ''
+    ln -s ${../../Cargo.lock} ${cargoRoot}/Cargo.lock
+  '';
 
-    doCheck = false;
+  doCheck = false;
 
-    meta = {
-      mainProgram = cargoToml.package.name;
-      platforms = lib.platforms.linux;
-    };
-  }
+  meta = {
+    mainProgram = cargoToml.package.name;
+    platforms = lib.platforms.linux;
+  };
+}
